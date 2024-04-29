5a56849030a3a4daa6b6636644f949ca956e5d0d
vandit <74812342+VanditAgarwal1@users.noreply.github.com>
2024-04-29 09:37:56 +0530

    Update runTestsAndGenerateReports.m

--- runTestsAndGenerateReports.m
+++ runTestsAndGenerateReports.m
@@ -1,45 +1,14 @@
-function runTestsAndGenerateReports(testFiles)
-    import matlab.unittest.TestRunner;
-    import matlab.unittest.plugins.XMLPlugin;
-    import matlab.unittest.TestSuite;
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.XMLPlugin;
+     
+suite = testsuite(pwd, 'IncludeSubfolders', true);
 
-    % Initialize a cell array to hold the test suites
-    suites = {};
+[~,~] = mkdir('test-results/matlab');
 
-    % Check if we are in the 'tests' directory
-    if isfolder('tests')
-        testFolder = 'tests'; % We are in the project root
-    else
-        testFolder = '.'; % We are in the 'tests' directory
-    end
+runner = TestRunner.withTextOutput();
+runner.addPlugin(XMLPlugin.producingJUnitFormat('test-results/matlab/results.xml'));
 
-    % Loop over each file and create a test suite
-    for i = 1:length(testFiles)
-        % Construct the full path to the test file, adding the .m extension
-        testFilePath = fullfile(testFolder, [testFiles{i}, '.m']);
+results = runner.run(suite);
+display(results);
 
-        % Append the test suite for each file to the cell array of test suites
-        suites{end+1} = TestSuite.fromFile(testFilePath);
-    end
-
-    % Concatenate all test suites into a single TestSuite array
-    suite = [suites{:}];
-
-    % Create a test runner with text output
-    runner = TestRunner.withTextOutput();
-
-    % Define the path for the results file
-    resultsFile = 'results.xml';
-    if isfolder('tests')
-        resultsFile = fullfile('tests', resultsFile);
-    end
-
-    % Add an XML plugin to produce JUnit-style test results
-    runner.addPlugin(XMLPlugin.producingJUnitFormat(resultsFile));
-
-    % Run the test suite
-    results = runner.run(suite);
-
-    % Display the test results
-    disp(results);
-end
+assertSuccess(results);
