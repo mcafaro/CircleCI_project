d23fdc772503c64e39e8c2f5fdab7f6ccac5d969
vandit <74812342+VanditAgarwal1@users.noreply.github.com>
2024-07-16 11:35:32 +0530

    Create test_example.py

---
+++ tests/test_example.py
@@ -0,0 +1,20 @@
+import unittest
+import xmlrunner
+
+class TestExample(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_subtraction(self):
+        self.assertEqual(5 - 3, 2)
+
+    def test_multiplication(self):
+        self.assertEqual(2 * 3, 6)
+
+    def test_division(self):
+        self.assertEqual(8 / 4, 2)
+
+if __name__ == "__main__":
+    with open('test-results/results_example.xml', 'wb') as output:
+        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output), failfast=False, buffer=False, catchbreak=False)
