0233dcadb8b76f4ce76d1012b05038645746a159
vandit <74812342+VanditAgarwal1@users.noreply.github.com>
2024-07-16 10:51:22 +0530

    Update run_all_tests.py

--- run_all_tests.py
+++ run_all_tests.py
@@ -5,8 +5,7 @@ import xmlrunner
 import test_example
 import test_math_operations
 import test_string_operations
-import test_list_operations
-import test_dictionary_operations
+
 
 # Create a test suite combining all test cases
 loader = unittest.TestLoader()
@@ -15,8 +14,6 @@ suite = unittest.TestSuite()
 suite.addTests(loader.loadTestsFromModule(test_example))
 suite.addTests(loader.loadTestsFromModule(test_math_operations))
 suite.addTests(loader.loadTestsFromModule(test_string_operations))
-suite.addTests(loader.loadTestsFromModule(test_list_operations))
-suite.addTests(loader.loadTestsFromModule(test_dictionary_operations))
 
 # Run the test suite and generate an XML report
 with open('test-results/all_tests_results.xml', 'wb') as output:
