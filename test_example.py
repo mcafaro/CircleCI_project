398a46b14d4ad4bae06a7bfacc7418abebba0bff
vandit <74812342+VanditAgarwal1@users.noreply.github.com>
2024-07-16 10:39:25 +0530

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -16,5 +16,5 @@ class TestExample(unittest.TestCase):
         self.assertEqual(8 / 4, 2)
 
 if __name__ == "__main__":
-    with open('results.xml', 'wb') as output:
+    with open('test-results/results.xml', 'wb') as output:
         unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output), failfast=False, buffer=False, catchbreak=False)
