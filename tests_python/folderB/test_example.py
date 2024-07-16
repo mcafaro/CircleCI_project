e2646b447d3bc04a9ea59a29a85da151eebbe886
vandit <74812342+VanditAgarwal1@users.noreply.github.com>
2024-07-16 12:04:53 +0530

    Update test_example.py

--- tests_python/folderB/test_example.py
+++ tests_python/folderB/test_example.py
@@ -8,19 +8,15 @@ class TestExample(unittest.TestCase):
         print("Running tests from folder B")
 
     def test_addition(self):
-        print("Running test_addition from folder B")
         self.assertEqual(1 + 1, 2)
 
     def test_subtraction(self):
-        print("Running test_subtraction from folder B")
         self.assertEqual(5 - 3, 2)
 
     def test_multiplication(self):
-        print("Running test_multiplication from folder B")
         self.assertEqual(2 * 3, 6)
 
     def test_division(self):
-        print("Running test_division from folder B")
         self.assertEqual(8 / 4, 2)
 
 if __name__ == "__main__":
